27fb2b6adbb6b7b2e84e5d0f6d269b147250b30d
igorleessa <igorleessa@gmail.com>
2023-09-25 13:11:17 -0300

    Adicionar arquivos de projeto.

---
+++ PrjBlocoIgorLessa.Test/ChuckNorrisControllerTest.cs
@@ -0,0 +1,16 @@
+using PrjBlocoIgorLessa.Controllers;
+
+namespace PrjBlocoIgorLessa.Test
+{
+    public class ChuckNorrisControllerTest
+    {
+        [Fact]
+        public void GetSuccess()
+        {
+            var controller = new ChuckNorrisController();
+
+            var result = controller.GetChuckNorrisRandom();
+            Assert.True(result.Count() > 0);
+        }
+    }
+}
